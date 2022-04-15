65f3627f9ab11f5b358d570fa77cb7d58cef1484
rexxinbot <102780669+rexxinbot@users.noreply.github.com>
2022-04-16 06:50:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,26 +13,26 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
+global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "RexxinBot" //namabot kalian
-global.ownername= "Rexxin" //nama kalian
+global.botname = "RexxinBotz" //namabot kalian
+global.ownername= "RexxinOfc" //nama kalian
 global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "aldomt724@gmail.com" //bebas
+global.youtube = "tiktok.com/@aldochris23" //bebas asal jan hapus
+global.github = "https://rexxinbot.github.io/" //bebas
+global.email = "rexxinofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.ownernomer = "6281317549796" // nomor wa kalian
-global.ownernomerr = "+6281317549796" //nmr wa kalian
+global.ownernomerr = "+6282145203493" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6281317549796","6281317549796","6282145203493"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Rexxin' //sticker wm ubah
-global.author = 'Di Buat Oleh Rexxin' //sticker wm ganti nama kalian
+global.background_welcome="https://telegra.ph/file/e3fe2eb2d09882d974ac6.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6281317549796","6282145203493","6282145203493"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© RexxinOfc' //sticker wm ubah
+global.author = 'Di Buat Oleh RexxinOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
